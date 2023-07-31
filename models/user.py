24509917091e0042321f88988118bfb21413c8e8
genyarko <115112347+genyarko@users.noreply.github.com>
2023-07-31 20:51:09 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -24,6 +24,22 @@ class User(BaseModel, Base):
         first_name = ""
         last_name = ""
 
+  @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        # Hash the password to MD5 before storing it
+        self._password = hashlib.md5(value.encode()).hexdigest()
+
+    def to_dict(self, include_password=False):
+        # Call the parent to_dict method and exclude password if needed
+        dictionary = super().to_dict()
+        if not include_password and 'password' in dictionary:
+            del dictionary['password']
+        return dictionary
+
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
