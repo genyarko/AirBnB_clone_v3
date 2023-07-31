93a7bc7ebec7f05d558822a477f1f02dda6842ed
genyarko <115112347+genyarko@users.noreply.github.com>
2023-07-31 20:35:33 +0200

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -15,3 +15,5 @@ from api.v1.views.places_reviews import *  # noqa
 from api.v1.views.users import *  # noqa
 from api.v1.views.amenities import *  # noqa
 from api.v1.views.places import *  # noqa
+from api.v1.views.places_amenities import * # noqa
+
