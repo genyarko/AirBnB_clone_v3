8df3ce5fe3fff06e7fd0224ec482a4c9b41e084a
genyarko <115112347+genyarko@users.noreply.github.com>
2023-08-01 00:39:22 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -20,6 +20,28 @@ else:
     Base = object
 
 
+#!/usr/bin/python3
+"""
+Contains class BaseModel
+"""
+
+from datetime import datetime
+import models
+from os import getenv
+import hashlib
+import sqlalchemy
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
+if models.storage_t == "db":
+    Base = declarative_base()
+else:
+    Base = object
+
+
 class BaseModel:
     """The BaseModel class from which future classes will be derived"""
     if models.storage_t == "db":
